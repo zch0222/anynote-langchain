e12f92d6fd682b6a6f600ec95d1a6302d11b3476
YXLM <yxlmzch0222@gmail.com>
2023-11-23 01:56:52 +0800

    测试

--- pdf.py
+++ pdf.py
@@ -21,7 +21,7 @@ dotenv.load_dotenv()
 print(os.environ["OPENAI_API_KEY"])
 
 
-loader = PyPDFLoader("/root/anynote-langchain/data/Android.pdf")
+loader = PyPDFLoader("/root/anynote-langchain/data/2022.pdf")
 docs = loader.load_and_split()
 
 print(docs)
