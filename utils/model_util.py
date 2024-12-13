813d9c253b46e1f194821da8d3bb9d67d00cceaa
称霸幼儿园 <yxlmzch@163.com>
2024-12-14 02:47:10 +0800

    update

--- utils/model_util.py
+++ utils/model_util.py
@@ -1,6 +1,8 @@
 from langchain_ollama.chat_models import ChatOllama
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
 
 def get_ollama_model(model: str):
     return ChatOllama(model=model)
@@ -11,6 +13,8 @@ def get_openai_model(model: str):
 def get_huggingface_embeddings(model: str):
     return HuggingFaceEmbeddings(model_name=model)
 
+def get_openai_embeddings(model: str):
+    return OpenAIEmbeddings(model=model)
 
 def get_model(model: str):
     split_model = model.split('_')
@@ -25,5 +29,7 @@ def get_embedding_model(model: str):
     print(split_model)
     if split_model[0] == "ollama":
         return get_ollama_model(split_model[1])
+    elif split_model[0] == "openai":
+        return get_openai_embeddings(split_model[1])
     elif split_model[0] == "huggingface":
         return get_huggingface_embeddings(split_model[1])
